f4ad5c067050b95afb7a228e50d70fdbac8193e3
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-17 17:56:25 -0400

    working on this soln, check discount

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require 'pry'
+class CashRegister
+    attr_accessor :discount, :total, :items
 
+    def initialize(discount=nil)
+        @total = 0.0
+        @items = []
+        @discount = discount.to_f
+    end
+
+    def add_item(item, cost, quantity=1)
+        @items << self
+        @total += cost * quantity
+    end
+
+    def apply_discount
+        if @discount == nil
+            return "There is no discount to apply."
+        else
+            @total *= (1-(@discount/100.0))
+            return "After the discount, the total comes out to #{@total}"
+        end
+        binding.pry
+    end
+end #END CashRegister
